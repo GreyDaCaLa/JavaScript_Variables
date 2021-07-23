438239a5759f7c7a3dc3eef74bc66d3d535acc07
GreyDaCaLa <gdele9164@gmail.com>
2021-07-22 21:17:31 -0400

    My Initial Commit -- Fixes

--- app.js
+++ app.js
@@ -5,6 +5,39 @@ console.log(
 
 // Exercise 1
 
+const firstName = 'Greg';
+let lastName = 'De Leon';
+var age = 26;
+
 // Exercise 2
 
+let fullName = firstName +' '+ lastName;
+console.log(fullName);
+    // Bonous
+      console.log(`litteral: ${firstName} ${lastName}`)
+
 // Exercise 3
+city = "G-Rap"
+passtime = 'Puzzles'
+
+let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passtime} and coding!`
+console.log(myStory)
+
+
+
+/*
+## Exercise 3: 
+
+1. Declare a variable named myStory using the `let` keyword, and assign a string with 3 sentences detailing:
+    - An introduction using your `fullName` variable.
+    - An introduction using your `city` variable.
+    - An introduction using your favorite `passtime` variable.
+2. Be sure to use variables as the above answers (declare and assign variables if you haven't already)
+3. Print the result of `myStory` to the browser console
+
+Ex: 
+```
+let myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passtime} and coding!`;
+```
+
+*/
